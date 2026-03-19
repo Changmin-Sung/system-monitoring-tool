482a7fe092cb220dd4eb4ba94db955aaa7adfb87
Changmin Sung <129953639+Changmin-Sung@users.noreply.github.com>
2026-03-20 02:14:53 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+CPU_THRESHOLD = 80
+RAM_THRESHOLD = 80
+DISK_THRESHOLD = 80
+
+CHECK_INTERVAL = 5  # seconds
+
+EMAIL_ALERT = False
+
+EMAIL_CONFIG = {
+    "sender": "your_email@gmail.com",
+    "password": "your_app_password",
+    "receiver": "target_email@gmail.com"
+}
